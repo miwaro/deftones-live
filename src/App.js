53ca50ebef5e2f9c8feedcc87b8db9f79354a2c0
Michael <mikerooze12@gmail.com>
2022-07-27 21:49:48 -0400

    Create HomeContent component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header.js';
-import DataTable from './components/DataTable.js';
+import HomeContent from './components/HomeContent.js';
 import Footer from './components/Footer.js';
 
 import './App.css';
@@ -8,21 +8,7 @@ function App() {
   return (
     <div className="App" >
       <Header />
-      <div id="common">
-        <div className="video-container">
-          <iframe width="100%" height="auto" src="https://www.youtube.com/embed/70wKncA9Kbc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        </div>
-        <h3>Deftones - Lovers (live debut)</h3>
-        <p>Moda Center<br />
-          Portland, OR<br />
-          USA<br />
-          April 14, 2022<br />
-          North American Spring tour<br /><br />
-
-          "Lovers"<br /><br />
-
-          On August 26, 2003, Deftones released the song "Lovers" as a b-side to the single CD of "Hexagram". It is on April 14, 2022, roughly 19 years later following the release of the single, that they decided to play the song live for the first time. </p>
-      </div>
+      <HomeContent />
       <Footer />
     </div >
   );
---
+++ src/components/HomeContent.js
@@ -0,0 +1,21 @@
+function HomeContent() {
+    return (
+        <div id="common">
+            <div className="video-container">
+                <iframe width="100%" height="auto" src="https://www.youtube.com/embed/70wKncA9Kbc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+            <h3>Deftones - Lovers (live debut)</h3>
+            <p>Moda Center<br />
+                Portland, OR<br />
+                USA<br />
+                April 14, 2022<br />
+                North American Spring tour<br /><br />
+
+                "Lovers"<br /><br />
+
+                On August 26, 2003, Deftones released the song "Lovers" as a b-side to the single CD of "Hexagram". It is on April 14, 2022, roughly 19 years later following the release of the single, that they decided to play the song live for the first time. </p>
+        </div>
+    )
+}
+
+export default HomeContent;
